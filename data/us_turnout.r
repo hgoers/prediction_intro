8718d7cd96d22ffac4bc013414d8bae7096ce913
Harriet Goers <h.goers@hotmail.com>
2025-10-27 14:46:18 -0400

    Update turnout data

--- data/us_turnout.r
+++ data/us_turnout.r
@@ -1,11 +1,15 @@
 library(tidyverse)
 library(janitor)
 
-us_turnout_raw <- read_csv("https://election.lab.ufl.edu/data-downloads/turnoutdata/Turnout_1980_2022_v1.2.csv")
+us_turnout_raw <- read_csv(
+  "https://election.lab.ufl.edu/data-downloads/turnoutdata/Turnout_1980_2022_v1.2.csv"
+)
 
-us_turnout <- us_turnout_raw |> 
-  clean_names() |> 
-  transmute(year, state, vep, 
-            vep_turnout_rate = as.numeric(str_remove(vep_turnout_rate, "%")))
+us_turnout <- us_turnout_raw |>
+  clean_names() |>
+  filter(state == "United States", year >= 1998) |>
+  transmute(year, turnout_rate = as.numeric(str_remove(vep_turnout_rate, "%")))
+
+us_turnout
 
 write_rds(us_turnout, here::here("data", "us_turnout.rds"))
